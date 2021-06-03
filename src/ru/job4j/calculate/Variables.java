61876fc131323b2aa647c2d33be420e6f22aeae0
progprog-1907 <progprog1907@gmail.com>
2021-06-03 01:11:09 -0700

    3.1. Загрузка файлов на github [#243565]

---
+++ src/ru/job4j/calculate/Variables.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculate;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        int length = 100500;
+        System.out.println(length);
+        long money = 900500;
+        System.out.println(money);
+        float weight = 1.05F;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+
+        // int age = 18;
+        // float length = 78.0332;
+        // boolean baby = true;
+    }
+}
