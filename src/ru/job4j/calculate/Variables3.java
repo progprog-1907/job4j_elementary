0adcce0456f5042b80f21f97576395336580e9e5
progprog-1907 <progprog1907@gmail.com>
2021-06-03 02:03:25 -0700

    3.1. Загрузка файлов на github [#243567]

---
+++ src/ru/job4j/calculate/Variables3.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculate;
+
+public class Variables3 {
+    public static void main(String[] args) {
+        byte age = 27; // для возраста
+        char gender = 'M'; // для пол-а
+        float height = 180.7f; // для роста
+        short bricks = 10000; // для количество кирпиджей
+        long bacterium = 12784216711049L; // для бактерий
+        int objeckts = 45000; // для объектов
+        boolean workPermit = false; // для разрешения на работу
+        double fileSize = 164797918.5; // для размера файла
+    }
+}
