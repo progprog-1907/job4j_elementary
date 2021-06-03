b181865148c17af08915d6fb907c9643fa8a2740
progprog-1907 <progprog1907@gmail.com>
2021-06-03 04:08:43 -0700

    3.1. Загрузка файлов на github [#243572]

---
+++ src/ru/job4j/calculate/MathFunc.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculate;
+
+public class MathFunc {
+    public static void main(String[] args) {
+        System.out.println(MathFunc.func1(100));
+    }
+
+    public static int func1(int x) {
+        int result = x;
+        return result;
+    }
+}
