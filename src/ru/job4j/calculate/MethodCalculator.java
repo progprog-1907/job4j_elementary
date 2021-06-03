8c1e27f95053014c784a45994d019f8f2dee39a7
progprog-1907 <progprog1907@gmail.com>
2021-06-03 02:25:31 -0700

    3.1. Загрузка файлов на github [#243570]

---
+++ src/ru/job4j/calculate/MethodCalculator.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculate;
+
+public class MethodCalculator {
+    public static void main(String[] args) {
+        MethodCalculator.plus(100, 500);
+        MethodCalculator.plus(4, 2);
+        MethodCalculator.plus(3, 5);
+    }
+
+    public static void plus(int x, int y) {
+        System.out.println(x + y);
+    }
+}
