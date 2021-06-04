7b0c19798a93a0815454291442cfdeb35ef7a080
progprog-1907 <progprog1907@gmail.com>
2021-06-04 00:10:48 -0700

    3.1. Загрузка файлов на github [#243577]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,7 +1,12 @@
 package ru.job4j;
 
 public class Main {
-    public static void main(String [] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+        long i = 129;
+        byte b = (byte) i;
+        short s = (short) i;
+        char c = 45000;
+        float f = c;
+        System.out.println(f);
     }
 }
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,20 +2,25 @@ package ru.job4j.converter;
 
 public class Converter {
     public static int rubleToEuro(int value) {
-        int rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static int rubleToDollar(int value) {
-        int rsl = value / 60; /* формула перевода рублей в доллары. */
-        return rsl;
+        return value / 60; /* формула перевода рублей в доллары. */
     }
 
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+        int inToDollar = 120;
+        int expectedToDollar = 2;
+        int outToDollar = Converter.rubleToDollar(inToDollar);
+        boolean passedToDollar = expectedToDollar == outToDollar;
+        System.out.println("120 Rubles are 2. Test result: " + passedToDollar);
 
-        int dollar = Converter.rubleToDollar(120);
-        System.out.println("120 rubles are " + dollar + " dollar");
+        int inToEuro = 140;
+        int expectedToEuro = 2;
+        int outToEuro = Converter.rubleToEuro(inToEuro);
+        boolean passedToEuro = expectedToEuro == outToEuro;
+        System.out.println("140 Euro's are 2. Test result: " + passedToEuro);
     }
+
 }
