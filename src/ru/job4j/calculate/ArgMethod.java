e153a7e9c843511d78719d6e5c028ecbc6af5682
progprog-1907 <progprog1907@gmail.com>
2021-06-04 01:53:37 -0700

    3.1. Загрузка файлов на github [#243579]

--- src/ru/job4j/calculate/ArgMethod.java
+++ src/ru/job4j/calculate/ArgMethod.java
@@ -3,18 +3,12 @@ package ru.job4j.calculate;
 public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name);
+        System.out.println("I am " + age + " years old.");
     }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
-
-        ArgMethod.hello(name, age);
-
-        ArgMethod.hello(name, age);
-
-        ArgMethod.hello(name, age);
-
         ArgMethod.hello(name, age);
     }
 }
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -10,8 +10,8 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        int inToDollar = 120;
-        int expectedToDollar = 2;
+        int inToDollar = 180;
+        int expectedToDollar = 3;
         int outToDollar = Converter.rubleToDollar(inToDollar);
         boolean passedToDollar = expectedToDollar == outToDollar;
         System.out.println("120 Rubles are 2. Test result: " + passedToDollar);
