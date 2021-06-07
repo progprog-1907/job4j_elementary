d3925a9e96b6e61514fc5288fbc610acc54203eb
progprog-1907 <progprog1907@gmail.com>
2021-06-07 03:32:37 -0700

    3.1. Загрузка файлов на github [#243596]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,8 +2,8 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static void main(String[] args) {
-        double result = square(4, 1);
-        System.out.println("p = 4, k = 1, s = 1, real = " + result);
+        double result = square(6, 2);
+        System.out.println("p = 6, k = 2, s = 2, real = " + result);
     }
 
     public static double square(int p, double k) {
