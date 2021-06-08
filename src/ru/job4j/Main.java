b780748c5302c28ccdfac063aee41974f2ac7af3
progprog-1907 <progprog1907@gmail.com>
2021-06-08 03:18:02 -0700

    3.1. Загрузка файлов на github [#243611]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -2,11 +2,6 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        long i = 129;
-        byte b = (byte) i;
-        short s = (short) i;
-        char c = 45000;
-        float f = c;
-        System.out.println(f);
+
     }
 }
