876fe738e9d65999c5eb7298abf51b8f19017efe
progprog-1907 <progprog1907@gmail.com>
2021-06-08 02:19:49 -0700

    3.1. Загрузка файлов на github [#243607]

---
+++ test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxTest {
+
+    @Test
+    public void maxLeft1Right2() {
+        int left = 1;
+        int right = 2;
+        int expected = 2;
+        int out = Max.max(left, right);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void maxLeft4Right0() {
+        int left = 4;
+        int right = 0;
+        int expected = 4;
+        int out = Max.max(left, right);
+        Assert.assertEquals(expected, out);
+    }
+}
