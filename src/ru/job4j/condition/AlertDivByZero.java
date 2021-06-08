6f5f3577cc50ec461ff2dcc3e4fd9667d88e726f
progprog-1907 <progprog1907@gmail.com>
2021-06-07 23:07:34 -0700

    3.1. Загрузка файлов на github [#243601]

--- src/ru/job4j/condition/AlertDivByZero.java
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -10,7 +10,9 @@ public class AlertDivByZero {
         boolean result = number == 0;
         if (result) {
             System.out.println("Could not div by 0.");
-        } else if (number < 0) {
+        }
+
+        if (number < 0) {
             System.out.println("This is negative numbers");
         }
     }
