d9abb72bad7602719d56f09797a7aac28ffcc557
progprog-1907 <progprog1907@gmail.com>
2021-06-14 00:50:18 -0700

    5.0.2. Вечный цикл for. [#226417]

---
+++ src/ru/job4j/loop/InfinityLoop.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class InfinityLoop {
+    public static void main(String[] args) {
+        for (int index = 0; index < 10; index++) {
+            System.out.println("infinity");
+            index = index - 1;
+            // из за index-- у нас цикл остается всегда на нуле по этому цикл не заканчивается!
+        }
+    }
+}
