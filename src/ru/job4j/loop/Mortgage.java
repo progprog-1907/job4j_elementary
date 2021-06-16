656de3ffad62fd9913d339b4fe9568ef92f06ca0
progprog-1907 <progprog1907@gmail.com>
2021-06-16 03:16:05 -0700

    6.1. Классический поиск перебором. [#33489]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,15 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        boolean credit = true;
 
-        while (credit) {
+        while (amount > 0) {
             amount += amount * percent / 100;
             amount -= salary;
             year++;
-            if (amount <= 0) {
-                credit = false;
-            }
         }
 
         return year;
