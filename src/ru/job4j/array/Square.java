66ed0d27ecf19d5f13cca9bab64ace880659ec2b
progprog-1907 <progprog1907@gmail.com>
2021-06-16 03:04:18 -0700

    6.0.5. Массивы в тестах. [#226789]

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -8,11 +8,4 @@ public class Square {
         }
         return rst;
     }
-
-    public static void main(String[] args) {
-        int[] array = calculate(4);
-        for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
-        }
-    }
 }
