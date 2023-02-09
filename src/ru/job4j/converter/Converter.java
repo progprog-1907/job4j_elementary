7d8031766ef5c0a8ae3b788141550f062ccce23e
progprog-1907 <progprog1907@gmail.com>
2023-02-10 02:31:44 +0400

    9.3. Математические операторы и составные операторы присваивания [#504905 #389307]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,12 +1,13 @@
 package ru.job4j.converter;
 
+
 public class Converter {
     public static int rubleToEuro(int value) {
         return value / 70;
     }
 
     public static int rubleToDollar(int value) {
-        return value / 60; /* формула перевода рублей в доллары. */
+        return value / 60;
     }
 
     public static void main(String[] args) {
@@ -22,5 +23,4 @@ public class Converter {
         boolean passedToEuro = expectedToEuro == outToEuro;
         System.out.println("140 Euro's are 2. Test result: " + passedToEuro);
     }
-
 }
